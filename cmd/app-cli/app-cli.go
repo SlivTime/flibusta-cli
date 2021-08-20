c629fbc5bc716a8f44ba7c20a3d24d63160ac622
Andrey Talantsev <avtalantsev@avito.ru>
2021-08-20 23:22:48 +0300

    Move cli part to separate cmd

---
+++ cmd/app-cli/app-cli.go
@@ -0,0 +1,91 @@
+package app_cli
+
+import (
+	"fmt"
+	"github.com/slivtime/flibusta-cli/pkg/client"
+	"github.com/urfave/cli/v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+const defaultBookFormat = "mobi"
+
+type FlibustaCLI struct{}
+
+func commandSearch(context *cli.Context) error {
+	query := strings.Join(context.Args().Slice(), " ")
+	flibusta, err := client.FromEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("search book: ", query)
+	searchResult, err := flibusta.Search(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, item := range searchResult {
+		fmt.Println(item.String())
+	}
+	return nil
+}
+
+func commandGet(context *cli.Context) error {
+	bookID := context.Args().First()
+
+	flibusta, err := client.FromEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	bookFormat := context.String("format")
+	fmt.Printf("get book <%s> in `%s` format\n", bookID, bookFormat)
+	result, err := flibusta.Download(bookID, bookFormat)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if result.Name == "" {
+		result.Name = fmt.Sprintf("%s.%s", bookID, bookFormat)
+	}
+	err = ioutil.WriteFile(result.Name, result.File, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("File saved at", result.Name)
+	return nil
+}
+
+func (c *FlibustaCLI) Start() (err error) {
+	app := &cli.App{
+		Commands: cli.Commands{
+			&cli.Command{
+				Name:    "search",
+				Aliases: []string{"s"},
+				Usage:   "Search book",
+				Action:  commandSearch,
+			},
+			&cli.Command{
+				Name:    "get",
+				Aliases: []string{"g"},
+				Usage:   "Get book",
+				Action:  commandGet,
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "format",
+						Aliases: []string{"f"},
+						Value:   defaultBookFormat,
+						Usage:   "Format to download: mobi|epub|fb2",
+						EnvVars: []string{"FLIBUSTA_PREFERRED_FORMAT"},
+					},
+				},
+			},
+		},
+	}
+
+	err = app.Run(os.Args)
+	if err != nil {
+		return
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,87 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"github.com/slivtime/flibusta-cli/pkg/client"
-	"github.com/urfave/cli/v2"
-	"io/ioutil"
+	"github.com/slivtime/flibusta-cli/cmd/app-cli"
 	"log"
-	"os"
-	"strings"
 )
 
-const defaultBookFormat = "mobi"
-
-func commandSearch(context *cli.Context) error {
-	query := strings.Join(context.Args().Slice(), " ")
-	flibusta, err := client.FromEnv()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("search book: ", query)
-	searchResult, err := flibusta.Search(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, item := range searchResult {
-		fmt.Println(item.String())
-	}
-	return nil
-}
-
-func commandGet(context *cli.Context) error {
-	bookID := context.Args().First()
-
-	flibusta, err := client.FromEnv()
-	if err != nil {
-		log.Fatal(err)
-	}
-	bookFormat := context.String("format")
-	fmt.Printf("get book <%s> in `%s` format\n", bookID, bookFormat)
-	result, err := flibusta.Download(bookID, bookFormat)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if result.Name == "" {
-		result.Name = fmt.Sprintf("%s.%s", bookID, bookFormat)
-	}
-	err = ioutil.WriteFile(result.Name, result.File, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("File saved at", result.Name)
-	return nil
-}
-
 func main() {
-	app := &cli.App{
-		Commands: cli.Commands{
-			&cli.Command{
-				Name:    "search",
-				Aliases: []string{"s"},
-				Usage:   "Search book",
-				Action:  commandSearch,
-			},
-			&cli.Command{
-				Name:    "get",
-				Aliases: []string{"g"},
-				Usage:   "Get book",
-				Action:  commandGet,
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:    "format",
-						Aliases: []string{"f"},
-						Value:   defaultBookFormat,
-						Usage:   "Format to download: mobi|epub|fb2",
-						EnvVars: []string{"FLIBUSTA_PREFERRED_FORMAT"},
-					},
-				},
-			},
-		},
-	}
-
-	err := app.Run(os.Args)
+	appCli := app_cli.FlibustaCLI{}
+	err := appCli.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
